e86ce33f314ce990b9338b70115fdfb4755619f5
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-10-25 20:58:34 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResouce.java
+++ src/main/java/com/educandoweb/course/resources/UserResouce.java
@@ -1,19 +1,40 @@
+/*
+ * Essa a a classe controladore REST
+ */
+
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResouce {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")  										// indica que a requisição recebera um id na url
+	public ResponseEntity<User> finfById(@PathVariable Long id){      	//anotation para que o spring saiba que se trata de um id que vera da url
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);   						// "ResponseEntity.ok" para indicar que teve sucesso
+	}
+	
+	
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service  										//registrara a classe com servico do String para injeção de dependencia
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); 							//retorna o objeto "user" que estiver dentro do optiol.
+	}
+
+}
