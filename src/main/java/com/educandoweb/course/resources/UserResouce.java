bae90488bbd344bcd372d31251713cc0fd1b5be9
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-11-11 15:46:00 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResouce.java
+++ src/main/java/com/educandoweb/course/resources/UserResouce.java
@@ -4,14 +4,18 @@
 
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -35,5 +39,11 @@ public class UserResouce {
 		return ResponseEntity.ok().body(obj);   						// "ResponseEntity.ok" para indicar que teve sucesso
 	}
 	
-	
+	@PostMapping  //esse metodo define no http que é uma inserção
+	public ResponseEntity<User> insert(@RequestBody User obj) {  //@RequestBody para o java saber que esse objeto vio da requisição e será desserializado para ser inserido no banco
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();															//gerendo URI esperado na requisição created que gera o codigo 201, que significa que um novo objeto/usuario foi criado, isso é visualisado la no json quando criado um no
+
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
