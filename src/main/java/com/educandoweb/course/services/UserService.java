809ce04a9b48cbaec9f887caf1d3546504612f56
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-11-25 16:20:26 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service  										//registrara a classe com servico do String para injeção de dependencia
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id); //objeto monitorado em memoria, éssa opção é melhor que utilizar o findById que vai até o bando de dados para buscar a informação tornando o processo mais lento
-		updateData(entity, obj);  //função criada
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id); //objeto monitorado em memoria, éssa opção é melhor que utilizar o findById que vai até o bando de dados para buscar a informação tornando o processo mais lento
+			updateData(entity, obj);  //função criada
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
